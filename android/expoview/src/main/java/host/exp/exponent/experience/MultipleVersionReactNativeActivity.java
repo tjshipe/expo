bdb24dc1c1ad8e6f5584ce9054d218b4ac548c61
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-06-01 16:29:46 +0200

    [TO BE REVERTED] - remove all, but once versioned SDKs

--- android/expoview/src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
+++ android/expoview/src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
@@ -7,13 +7,6 @@ public class MultipleVersionReactNativeActivity extends ReactNativeActivity impl
     // The 4-space indentation is used by android-build-aar.sh.
     // WHEN_DISTRIBUTING_REMOVE_FROM_HERE
     // WHEN_PREPARING_SHELL_REMOVE_FROM_HERE
-    // BEGIN_SDK_35
-    abi35_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
-    // END_SDK_35
-    // BEGIN_SDK_36
-    abi36_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
-    abi36_0_0.com.facebook.react.modules.core.PermissionAwareActivity,
-    // END_SDK_36
     // BEGIN_SDK_37
     abi37_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
     abi37_0_0.com.facebook.react.modules.core.PermissionAwareActivity,
@@ -25,12 +18,6 @@ public class MultipleVersionReactNativeActivity extends ReactNativeActivity impl
 
     // WHEN_DISTRIBUTING_REMOVE_FROM_HERE
     // WHEN_PREPARING_SHELL_REMOVE_FROM_HERE
-    // BEGIN_SDK_36
-    @Override
-    public void requestPermissions(String[] strings, int i, abi36_0_0.com.facebook.react.modules.core.PermissionListener permissionListener) {
-      super.requestPermissions(strings, i, permissionListener::onRequestPermissionsResult);
-    }
-    // END_SDK_36
     // BEGIN_SDK_37
     @Override
     public void requestPermissions(String[] strings, int i, abi37_0_0.com.facebook.react.modules.core.PermissionListener permissionListener) {
